8de45a17198016370009d9f23aeffb57b8a5e72e
jyotsanashyama <63206848+jyotsanashyama@users.noreply.github.com>
2020-04-06 00:49:58 +0530

    Added code for finding vowels & consonants

---
+++ vowels.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+temp=name[0]
+if temp=='a' or temp=='e' or temp=='i' or temp=='o' or temp=='u' or temp=='A' or temp=='E' or temp=='I' or temp=='O' or temp=='U':
+    print("The first letter of",name,"is a vowel.")
+else:
+    print("The first letter of",name,"is a consonant.")
