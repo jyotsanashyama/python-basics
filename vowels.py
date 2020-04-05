d71156d70fdadc722a192b984e9bff66401ab894
montyshyama <montyshyama031@gmail.com>
2020-04-06 01:37:06 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,4 +1,4 @@
-name=input("Enter your name:")
+name=input("Enter your name: ")
 temp=name[0]
 if temp=='a' or temp=='e' or temp=='i' or temp=='o' or temp=='u' or temp=='A' or temp=='E' or temp=='I' or temp=='O' or temp=='U':
     print("The first letter of",name,"is a vowel.")
